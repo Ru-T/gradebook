a84a395520b81f3e4f8c1a6b62a1873ce83ecbbf
Ru-T <rwajnberg@gmail.com>
2015-10-07 16:21:10 -0400

    Attempt to fix my controller tests, failed miserably.

--- test/controllers/teachers_controller_test.rb
+++ test/controllers/teachers_controller_test.rb
@@ -5,45 +5,50 @@ class TeachersControllerTest < ActionController::TestCase
     @teacher = teachers(:one)
   end
 
+  def setup
+    session[:user_id] = 1
+    session[:user_type] = "Teacher"
+  end
+
   test "should get index" do
     get :index
     assert_redirected_to session_new_path
     assert_not_nil assigns(:teachers)
   end
 
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
-  # test "should create teacher" do
-  #   assert_difference('Teacher.count') do
-  #     post :create, teacher: { email: @teacher.email, name: @teacher.name, password: @teacher.password }
-  #   end
-  #
-  #   assert_redirected_to teacher_path(assigns(:teacher))
-  # end
-  #
-  # test "should show teacher" do
-  #   get :show, id: @teacher
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit, id: @teacher
-  #   assert_response :success
-  # end
-  #
-  # test "should update teacher" do
-  #   patch :update, id: @teacher, teacher: { email: @teacher.email, name: @teacher.name, password_digest: @teacher.password_digest }
-  #   assert_redirected_to teachers_new_path(assigns(:teacher))
-  # end
-  #
-  # test "should destroy teacher" do
-  #   assert_difference('Teacher.count', -1) do
-  #     delete :destroy, id: @teacher
-  #   end
-  #
-  #   assert_redirected_to teachers_path
-  # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create teacher" do
+    assert_difference('Teacher.count') do
+      post :create, teacher: { email: @teacher.email, name: @teacher.name, password_digest: @teacher.password_digest }
+    end
+
+    assert_redirected_to teacher_path(assigns(:teacher))
+  end
+
+  test "should show teacher" do
+    get :show, id: @teacher
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @teacher
+    assert_response :success
+  end
+
+  test "should update teacher" do
+    patch :update, id: @teacher, teacher: { email: @teacher.email, name: @teacher.name, password_digest: @teacher.password_digest }
+    assert_redirected_to teachers_new_path(assigns(:teacher))
+  end
+
+  test "should destroy teacher" do
+    assert_difference('Teacher.count', -1) do
+      delete :destroy, id: @teacher
+    end
+
+    assert_redirected_to teachers_path
+  end
 end
--- test/integration/login_logout_test.rb
+++ test/integration/login_logout_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class LoginLogoutTest < ActionDispatch::IntegrationTest
 
-  # test "login page works" do
-  #   # get '/session/new' # This should become the root when you uncomment the below redirection tests
-  #   # # assert_redirected_to session_new_path # Testing that you got to login from a redirect
-  #   # # follow_redirect!
-  #   # # assert_template "new" # Testing that it rendered the new page
-  #   # assert response.body.include?("Password")
-  #   # post login_path, session: {email: "gibberish@example.com", password: "(%$%$%#)"}
-  #   # assert_redirected_to session_new_path # login page, testing that login failed
-  #   # post login_path, session: {email: "mason@example.com", password: "(catsdrool)"} # real info from fixture!
-  #   # assert_redirected_to teachers_path # destination page after successful log in
-  #   # follow_redirect!
-  #   # assert_select "article" # This is testing that there is one or more articles (rows) on the page. If you expect to see one, you can say "article", 1
-  #   # assert_select "tr", 2 # how many tables you expect, i.e. one per teacher
-  # end
-  #
-  # test "can create students" do
-  #   # post login_path, session: {email: "mason@example.com", password: "(catsdrool)"} # real info from fixture!
-  #   # post students_path, student: {name: "StudentName", email: "", password: "()", teacher_id: 1} # hit create action to make a new student
-  #   # get students_path
-  #   # count number of rows again
-  # end
+  test "login page works" do
+    get '/session/new' # This should become the root when you uncomment the below redirection tests
+    assert_redirected_to session_new_path # Testing that you got to login from a redirect
+    follow_redirect!
+    assert_template "new" # Testing that it rendered the new page
+    assert response.body.include?("Password")
+    post login_path, session: {email: "gibberish@example.com", password: "(%$%$%#)"}
+    assert_redirected_to session_new_path # login page, testing that login failed
+    post login_path, session: {email: "mason@example.com", password: "(catsdrool)"} # real info from fixture!
+    assert_redirected_to teachers_path # destination page after successful log in
+    follow_redirect!
+    assert_select "article" # This is testing that there is one or more articles (rows) on the page. If you expect to see one, you can say "article", 1
+    assert_select "tr", 2 # how many tables you expect, i.e. one per teacher
+  end
+
+  test "can create students" do
+    post login_path, session: {email: "mason@example.com", password: "(catsdrool)"} # real info from fixture!
+    post students_path, student: {name: "StudentName", email: "", password: "()", teacher_id: 1} # hit create action to make a new student
+    get students_path
+    assert_select "tr", 1
+  end
   #
   #   # assert_select "a[href=#{logout_path}]", 1 #checks if there is a logout link on the page
   #
