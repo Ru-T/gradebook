49bb28dbd67cfb9e8936b0bddb31fa9475ea82b3
Ru-T <rwajnberg@gmail.com>
2015-10-07 14:04:23 -0400

    Added migration to put date column on grades table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006183103) do
+ActiveRecord::Schema.define(version: 20151007180204) do
 
   create_table "grades", force: :cascade do |t|
     t.string   "assignment_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151006183103) do
     t.integer  "student_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.date     "date"
   end
 
   create_table "parents", force: :cascade do |t|
